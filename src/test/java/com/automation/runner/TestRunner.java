83c4c5b055bea74b55191efb1ed53458ddf0014f
chiragkhimani <khimanichirag@gmail.com>
2021-12-13 23:08:15 +0530

    Updated framework

--- src/test/java/com/automation/runner/TestRunner.java
+++ src/test/java/com/automation/runner/TestRunner.java
@@ -9,12 +9,6 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 				features = "src\\test\\resources\\features", 
 				glue = "com.automation.stepdef",
-				monochrome = true,
-				strict = true,
-<<<<<<< HEAD
-				tags="@wip",
-=======
->>>>>>> fae1e18ca313a764e8e7b9244d4ee3592f41099a
 				plugin = { "json:target/cucumber.json"}
 		)
 
